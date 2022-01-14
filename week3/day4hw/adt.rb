1f57b421040f1068550a58d481d1412e13dd70dd
Ras Ally <rasally434@gmail.com>
2022-01-14 11:30:07 -0500

    Add map class

--- week3/day4hw/adt.rb
+++ week3/day4hw/adt.rb
@@ -37,5 +37,43 @@ class Queue
     def peek
         @curr_queue[-1]
     end
-
 end
+
+class Map
+
+    def initialize
+        @curr_map = Array.new
+    end
+
+    def set(key, val)
+        # if @curr_map.none?{ |subarr| subarr.include?(key) }
+             @curr_map << [key, val]
+        # end
+    end
+
+    def get(key)
+        @curr_map.each { |subarr| return "#{key}=>#{subarr[1]}" if subarr[0] == key  }
+    end
+
+    def delete(key)
+      @curr_map.each_with_index do |subarr, i|
+        if subarr.include?(key)
+            @curr_map.delete_at(i)
+        end
+      end
+    end
+
+    def show
+        @curr_map.each do |subarr|
+            puts "#{subarr[0]}=>#{subarr[1]}"
+        end
+    end
+end
+
+map = Map.new
+p map.set('key_one', 'val_one')
+p map.set('key_two', 'val_two')
+p map.set('key_three', 'val_three')
+# p map.get('key_two')
+p map.delete('key_two')
+p map.show
